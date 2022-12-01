cc2d34fc65968c486be00fb881f6b69551fa07d5
omanias <ojmanias1985@gmail.com>
2022-12-01 18:52:05 -0300

    clase 7 agregada

---
+++ scripts/clase7.js
@@ -0,0 +1,93 @@
+/*
+function primero(segundo) {
+  setTimeout(function () {
+    console.log("PRMERO");
+    segundo();
+  }, 3000);
+}
+
+function segundo() {
+  console.log("SEGUNDO");
+}
+
+primero(segundo);
+*/
+
+// const pendientes = ["desafios", "preentregas", "asistencias", "actividades"];
+/*
+pendientes.forEach((p, i) => {
+  console.log(`${i} : ${p}`);
+});
+*/
+
+/* pendientes.map((p, i) => {
+  console.log(`${i} : ${p}`);
+}); */
+
+/*
+let total = 0;
+
+carrito.forEach((producto) => {
+    total += producto.precio;
+});
+
+console.log(total);
+*/
+/*
+let resultado = carrito.reduce((accum, producto) => {
+    return accum + producto.precio;
+}, 0);
+
+console.log(resultado);
+*/
+/*
+const carrito = [
+  { nombre: "Monitor", precio: 1000 },
+  { nombre: "Television", precio: 2000 },
+  { nombre: "Tablet", precio: 500 },
+  { nombre: "Auriculares", precio: 300 },
+  { nombre: "Teclado", precio: 200 },
+];
+*/
+/*
+let resultado = carrito.filter((p) => {
+  return p.precio >= 500;
+});
+*/
+/*
+let resultado = carrito.filter((p) => {
+  return p.nombre !== "Monitor";
+});
+console.log(resultado);
+*/
+/*
+const filtrado = carrito.find((p) => {
+  return p.nombre === "Mouse";
+});
+
+console.log(filtrado);
+*/
+
+//METODO MATH
+
+//console.log(Math.max(55, 30, 0, 40));
+//console.log(Math.min(55, 30, 0, 40));
+
+/* let pi = Math.PI;
+
+console.log(Math.ceil(pi));
+console.log(Math.floor(pi));
+console.log(Math.round(pi)); */
+
+// console.log(Math.sqrt(9));
+
+// console.log(Math.random());
+
+// console.log(new Date());
+/*
+let fecha = new Date();
+
+console.log(fecha.getFullYear());
+console.log(fecha.getMonth());
+console.log(fecha.getDay());
+*/
