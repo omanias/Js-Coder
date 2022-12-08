eb790e3dabde36264047b4a8367aa7b46e9f9086
omanias <ojmanias1985@gmail.com>
2022-12-07 22:32:01 -0300

    clase 9 agregada

---
+++ scripts/clase9.js
@@ -0,0 +1,80 @@
+//let containerProducts = document.querySelector(".container-products");
+
+/* containerProducts.addEventListener("click", () => {
+  console.log("click en nav");
+}); */
+
+/* containerProducts.addEventListener("mouseenter", () => {
+  console.log("dentro del contenedor");
+}); */
+
+/* containerProducts.addEventListener("mouseleave", () => {
+  console.log("fuera del contenedor");
+}); */
+
+/* containerProducts.addEventListener("dblclick", () => {
+  console.log("click doble");
+}); */
+
+// let buscador = document.querySelector("#buscador");
+
+/* buscador.addEventListener("keydown", () => {
+  console.log("Escribiendo...");
+}); */
+
+/* buscador.addEventListener("keyup", () => {
+  console.log("Escribiendo...");
+}); */
+
+/* buscador.addEventListener("copy", () => {
+  console.log("Copiado...");
+}); */
+
+/* buscador.addEventListener("paste", () => {
+  console.log("Pegado...");
+}); */
+
+/* buscador.addEventListener("input", (e) => {
+  // console.log(e.target);
+  // console.log(e.target.value);
+  if (e.target.value === "") {
+    console.log("No puede ingresar espacios");
+  }
+}); */
+
+//const formulario = document.querySelector("#formulario");
+
+/*
+formulario.addEventListener("submit", (e) => {
+  //console.log(e); // Aqui se refresca realmente la pagina
+  e.preventDefault(); // Evita que se refresque la pagina
+  console.log("Enviando formulario");
+  console.log(e.target.action);
+});
+ */
+
+//EJEMPLO DE VALIDACION DE FORMULARIO
+/*
+const nombre = document.querySelector("#nombre");
+const correo = document.querySelector("#correo");
+formulario.addEventListener("submit", validarFormulario);
+
+function validarFormulario(e) {
+  e.preventDefault();
+  console.log(`Nombre: ${nombre.value}`);
+  console.log(`Correo: ${correo.value}`);
+}
+*/
+
+//EXTRAYENDO VALORES
+/*
+let cardTitle = document.querySelector(".card-title");
+let cardText = document.querySelector(".card-text");
+
+let boton = document.querySelector("#boton1");
+
+boton.addEventListener("click", function () {
+  console.log(cardTitle.textContent);
+  console.log(cardText.textContent);
+});
+*/
